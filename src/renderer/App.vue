4b9db4a532e83eaa0ca33f5e1c4f1fd8029da0ab
u9lyfish <u9lyfish@gmail.com>
2019-09-13 16:44:49 +0800

    Initiate project with electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'handy-hosts'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
